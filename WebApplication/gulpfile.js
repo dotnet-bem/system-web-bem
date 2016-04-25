05959de351dd851d993c9fc02e2da3f416de8414
dima117 <dima117a@gmail.com>
2016-04-25 12:44:52 +0400

    test projects

---
+++ WebApplication/gulpfile.js
@@ -0,0 +1,20 @@
+﻿var gulp = require('gulp');
+var concat = require('gulp-concat');
+var del = require('del');
+
+var config = {
+    src: ['Bem/desktop.blocks/**/*.js']
+}
+
+gulp.task('clean', function () {
+    return del(['Bem/desktop.bundles/**/*.js']);
+});
+
+gulp.task('scripts', ['clean'], function () {
+
+    return gulp.src(config.src)
+      .pipe(concat('all.min.js'))
+      .pipe(gulp.dest('app/'));
+});
+
+gulp.task('default', ['scripts'], function () { });
